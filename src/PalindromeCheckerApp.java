e3c217726edfe01b1ae341f85933955edfb75a28
new-cde <abinaya.muthukrishnan.3006@gmail.com>
2026-02-27 16:23:39 +0530

    UC7:Deque-Based Optimized Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,34 +1,31 @@
-import java.util.Queue;
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Stack;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "level";
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
+        String input = "racecar";
+        Deque<Character> deque = new LinkedList<>();
 
         for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            queue.add(ch);     // Enqueue
-            stack.push(ch);    // Push
+            deque.addLast(input.charAt(i));  // Insert at rear
         }
 
         boolean isPalindrome = true;
-        while (!queue.isEmpty()) {
-            char fromQueue = queue.remove();   // Dequeue
-            char fromStack = stack.pop();      // Pop
 
-            if (fromQueue != fromStack) {
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();  // Remove from front
+            char rear = deque.removeLast();    // Remove from rear
+            if (front != rear) {
                 isPalindrome = false;
                 break;
             }
         }
 
         System.out.println("Input String : " + input);
+
         if (isPalindrome) {
             System.out.println("Result : It is a Palindrome.");
         } else {
-            System.out.println("Result : It is NOT a Palindrome.");
+                System.out.println("Result : It is NOT a Palindrome.");
         }
     }
 }
