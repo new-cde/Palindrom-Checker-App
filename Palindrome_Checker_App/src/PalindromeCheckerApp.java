80f2009a0ad08694f4622ee1316f6d3fd3dc74aa
new-cde <abinaya.muthukrishnan.3006@gmail.com>
2026-03-09 16:01:39 +0530

    UC11:Object-Oriented Palindrome Service

--- Palindrome_Checker_App/src/PalindromeCheckerApp.java
+++ Palindrome_Checker_App/src/PalindromeCheckerApp.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
-public class PalindromeCheckerApp {
+class PalindromeChecker {
+    public boolean checkPalindrome(String str) {
 
-    public static boolean isPalindrome(String str) {
         int start = 0;
         int end = str.length() - 1;
 
@@ -14,20 +14,25 @@ public class PalindromeCheckerApp {
         }
         return true;
     }
+}
+
+
+// Main application class
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Palindrome Checker App - Case Insensitive & Space Ignored");
+        System.out.println("Palindrome Checker App - Object Oriented Approach");
         System.out.print("Enter a string: ");
 
         String input = scanner.nextLine();
 
-        // Normalize the string
-        String normalized = input.toLowerCase().replaceAll("\\s+", "");
+        // Create object of service class
+        PalindromeChecker checker = new PalindromeChecker();
 
-        boolean result = isPalindrome(normalized);
+        boolean result = checker.checkPalindrome(input);
 
         if (result) {
             System.out.println("The given string is a palindrome.");
